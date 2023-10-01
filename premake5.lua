910686dd09ff8e6d952ebf22965606bc9d9c7ec6
Revoke <suhenich.max@gmail.com>
2023-10-01 17:06:56 +0300

    Added basic premake file

---
+++ premake5.lua
@@ -0,0 +1,101 @@
+workspace "MyRevoke"
+    architecture "x64"
+
+    configurations { "Debug", "Release" }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    startproject "Sandbox"
+
+    project "MyRevoke"
+        location "MyRevoke"
+        kind "SharedLib"
+        language "C++"
+
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp",
+        }
+
+        includedirs
+        {
+            "%{prj.name}/vendor/spdlog/include"
+        }
+
+        filter "system:windows"
+            cppdialect "C++20"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines
+            {
+                "RV_PLATFORM_WINDOWS",
+                "RV_BUILD_DLL"
+            }
+
+            postbuildcommands
+            {
+               ("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+            }
+
+        filter "configurations:Debug"
+            defines "RV_DEBUG"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines "RV_RELEASE"
+            optimize "On"
+
+        filter { "system:windows", "configurations:Release" }
+            buildoptions "/MT"
+
+project "SandBox"
+    location "SandBox"
+    kind "ConsoleApp"
+
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "MyRevoke/vendor/spdlog/include",
+        "MyRevoke/src"
+    }
+
+    links
+    {
+        "MyRevoke"
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "RV_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "RV_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "RV_RELEASE"
+        optimize "On"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
